79776b63518ec04925c3348187cbc4a17b446a3c
Kai <zengkai2009@foxmail.com>
2016-10-29 16:49:36 +0800

    update async

--- es6/async-await.js
+++ es6/async-await.js
@@ -13,11 +13,44 @@
 //console.log(g.next()) // { value: 3, done: false }
 //console.log(g.next()); // { value: undefined, done: true }
 
-async function f() {
-	throw new Error('error')
+
+//async function f() {
+//	throw new Error('error')
+//}
+//
+//f().then(
+//	v => console.log('v', v),
+//	e => console.log('e', e)
+//)
+
+//const fetch = require('node-fetch')
+//
+//async function getUrl(url) {
+//	try {
+//		const response = await fetch(url)
+//		const html = await response.text()
+//		
+//		return html.match(/<title>([\s\S]+)<\/title>/i)[1]
+//	} catch (e) {
+//		console.log('e')
+//	}
+//}
+//
+//
+//getUrl('http://www.baidu.com').then(console.log)
+
+//async function delay(time) {
+//	return await new Promise(resolve => setTimeout(() => resolve(), time))
+//}
+
+//delay(2000).then(() => console.log(2222))
+
+const delay = time => new Promise(resolve => setTimeout(resolve, time))
+
+const test = async () => {
+	await delay(2000)
+	
+	return 22222
 }
 
-f().then(
-	v => console.log('v', v),
-	e => console.log('e', e)
-)
+test().then(console.log)
