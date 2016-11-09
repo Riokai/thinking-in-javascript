76882ec97f9f82cab89c58de6403cf1ed0270cac
Kai <zengkai2009@foxmail.com>
2016-11-09 17:55:05 +0800

    test demo app

--- framework/react/react-app/src/App.js
+++ framework/react/react-app/src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Demo from './Demo'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      num: 0
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        num: this.state.num + 1
+      })
+    }, 1000)
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header" style={{ marginBottom: 20}}>
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <Demo num={this.state.num} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
