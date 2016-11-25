0fe47718d440a9e15970d7abca4d65893e486849
Kai <zengkai2009@foxmail.com>
2016-11-25 10:05:33 +0800

    vue list

--- framework/vue/src/App.vue
+++ framework/vue/src/App.vue
@@ -2,6 +2,16 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <div id="app-3">
+      <p v-if="seen">Now you see me</p>
+    </div>
+    <div id="app-4">
+      <ol>
+        <li v-for="todo in todos">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
   </div>
 </template>
 
@@ -12,6 +22,16 @@ export default {
   name: 'app',
   components: {
     Hello
+  },
+  data () {
+    return {
+      seen: true,
+      todos: [
+        { text: 'Learn Javascript' },
+        { text: 'Learn Vue' },
+        { text: 'Learn React' }
+      ]
+    }
   }
 }
 </script>
